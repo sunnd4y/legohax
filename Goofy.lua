f56b51558f8154be997f67df20cb47ad867e796b
sunnd4y <120350465+sunnd4y@users.noreply.github.com>
2024-09-17 21:42:41 +0700

    Create Goofy.lua

---
+++ Goofy.lua
@@ -0,0 +1,30 @@
+local Players = game:GetService("Players");
+local RunService = game:GetService("RunService");
+local Client = Players.LocalPlayer;
+
+local Utility = {} 
+local connections = {}
+
+function Utility.Is_Alive(Entity)
+   return Entity and Entity.Character and Entity.Character.HumanoidRootPart
+end
+
+function Utility.Add_Connection(event, callback)
+    local connection = event:Connect(callback)
+    table.insert(connections, connection)
+    return connection
+end
+
+
+Utility.Add_Connection(RunService.PreRender, function()
+    
+    if Utility.Is_Alive(Client) then 
+       Client:Kick("You Being Alive?")
+    else 
+       Client:Kick("You Died?")
+    end
+
+    
+    
+end)
+
