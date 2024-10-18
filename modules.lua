2ced9b2c8f59f94c7f52bccdf8f8ee00961f4884
sunnd4y <120350465+sunnd4y@users.noreply.github.com>
2024-10-18 13:22:51 +0700

    Update modules.lua

--- modules.lua
+++ modules.lua
@@ -4,22 +4,29 @@
 
     local Modules = {}
 
-    function Modules.Output(type: number | string, text: string)
+do -- // Compresss code / Put them inside scope so the variable aint go no where (might help reduce garbage collection or reduce cache)
+    do -- // also leave the comments on each scope so you can comeback and fix it easier
+        function Modules.Output(type: number | string, text: string)
         local OutputType
         if type == 1 or type:lower() == "print" then
             OutputType = print
         elseif type == 2 or type:lower() == "warn" then
             OutputType = warn
-        elseif type == 3 or type:lower() == "error" then
+          elseif type == 3 or type:lower() == "error" then
             OutputType = error
-        else
+           else
             OutputType = print -- // Default to print if the type is invalid
-        end
-        return OutputType("[Sunnd4y Output] : " .. text)
-    end
-    
+          end
+          return OutputType("[Sunnd4y Output] : " .. text)
+       end
+    end; -- use ; as mark that its the end of scope so it will easier to read
+
+    do -- // Main
+       -- // Example usage
+
+        Modules.Output(1, "Hello, This is print")
+    end;
+end 
 
 
--- // Example usage
 
-Modules.Output(1, "Hello, This is print")
