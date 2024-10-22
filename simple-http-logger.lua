b86f95815f0324f689f64a99c867635656752236
sunnd4y <120350465+sunnd4y@users.noreply.github.com>
2024-10-22 16:42:28 +0700

    Update simple-http-logger.lua

--- simple-http-logger.lua
+++ simple-http-logger.lua
@@ -1,7 +1,7 @@
 -- // Support solara & very useful for cracking whitelist
 local OldReq = request or http.request
 
-local req = function(options)
+getgenv().request = function(options)
     print("Logging request to:", options.Url)
     print("Method:", options.Method)
     print("Headers:", options.Headers)
@@ -9,3 +9,21 @@ local req = function(options)
     return OldReq(options)
 end
 
+getgenv().http.request = function(options)
+    print("Logging request to:", options.Url)
+    print("Method:", options.Method)
+    print("Headers:", options.Headers)
+    print("Body:", options.Body)
+    return OldReq(options)
+end
+
+local response = request({
+    Url = "http://httpbin.org/post",  
+    Method = "POST",
+    Headers = {
+        ["Content-Type"] = "application/json"  
+    },
+    Body = game:GetService("HttpService"):JSONEncode({hello = "world"})
+})
+
+print("Response:", response)
